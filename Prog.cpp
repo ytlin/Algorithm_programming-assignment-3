80457fb64be1905b4707c068b96ed7a3e9860e72
ytlin <eq180aaa@hotmail.com.tw>
2016-05-08 22:19:14 +0800

    finish(First version)

--- Prog.cpp
+++ Prog.cpp
@@ -15,8 +15,17 @@ int main(void)
            cin.putback(tmp);
            cin>>N[n++];
       }
-      for(int i=0;i<n;i++)
+      
+      int max = 0;
+      int sum = 0;
+      for(int i=0 ; i<n ; i++)
       {
-            cout<<N[i]<<" ";
+         sum += N[i];
+         if(sum < 0) sum = 0;
+         else if(sum > max) max = sum;
       }
+
+      cout<<max<<endl;
+
+
 }
